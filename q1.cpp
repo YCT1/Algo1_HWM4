5bd3ad7e65874a508fedd932b2428587f7477f86
Yekta Can Tursun <yektacan1999@gmail.com>
2021-02-09 12:37:30 +0300

    Question 1 is submitted

--- q1.cpp
+++ q1.cpp
@@ -1,27 +1,34 @@
+/*
+Name: Yekta Can Tursun
+Number: 150170105
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
-
+//Data class is created in order to store necacary data in the store
+//Data objects can be used with comparasion operators, its usage can be change with operationMode
 class Data{
 
     private:
-        char key;
-        int x,y;
-        char operationMode;
+        char key;//Z, char data
+        int x,y;//X and Y int data
+        char operationMode; //Operation Mode
     
     public:
-        Data();
-        Data(int x, int y, char keys, char operationMode);
-        Data(const Data &p);
-        void copy(Data &p);
-        string get(){return "(" + to_string(x) + "," + to_string(y) + "," + key + ")";}
-        void set(int x, int y, char keys, char operationMode);
+        Data(); //General Constructor
+        Data(int x, int y, char keys, char operationMode); //Set mode
+        Data(const Data &p);//Copy constructor
+        void copy(Data &p);//Copy function
+        string get(){return "(" + to_string(x) + "," + to_string(y) + "," + key + ")";}//Returns string of data
+        void set(int x, int y, char keys, char operationMode); //Sets value of data
         
-        int getx(){return x;}
-        int gety(){return y;}
-        char getkey(){return key;}
-        char geto(){return operationMode;}
+        int getx(){return x;}//Get X data
+        int gety(){return y;}//Get Y data
+        char getkey(){return key;}//Get key
+        char geto(){return operationMode;}//Get Operation Mode
 
+        //This is the where comparision operators are created
         bool operator==(Data i);
         bool operator<=(Data i);
         bool operator>=(Data i);
@@ -62,6 +69,7 @@ void Data::set(int x, int y, char key, char operationMode){
 
 
 //Operator Overloding
+//The behavour of the comparision will be changed according to operation Mode
 bool Data::operator==(Data i){
     if(operationMode == 'x'){
         return x == i.x;
@@ -70,6 +78,7 @@ bool Data::operator==(Data i){
     }else if(operationMode == 'z'){
         return key == i.key;
     }
+    //If there is another operation mode that have not been writien print error
     cout << "error"<< endl;
     return false;
 }
@@ -82,6 +91,7 @@ bool Data::operator<=(Data i){
     }else if(operationMode == 'z'){
         return key <= i.key;
     }
+    //If there is another operation mode that have not been writien print error
     cout << "error"<< endl;
     return false;
 }
@@ -94,6 +104,7 @@ bool Data::operator>=(Data i){
     }else if(operationMode == 'z'){
         return key >= i.key;
     }
+    //If there is another operation mode that have not been writien print error
     cout << "error"<< endl;
     return false;
 }
@@ -106,6 +117,7 @@ bool Data::operator>(Data i){
     }else if(operationMode == 'z'){
         return key > i.key;
     }
+    //If there is another operation mode that have not been writien print error
     cout << "error"<< endl;
     return false;
 }
@@ -118,13 +130,17 @@ bool Data::operator<(Data i){
     }else if(operationMode == 'z'){
         return key < i.key;
     }
+    //If there is another operation mode that have not been writien print error
     cout << "error"<< endl;
     return false;
 }
+
+
+
 class Node{
 
     private:
-        Data *data; //The array of keys
+        Data *data; //The array of data
         int degree; //Minumum degree
         bool leaf; // If this is true, this node is leaf and end point,
         Node **childs; // Array of pointer to this nodes children
@@ -132,13 +148,13 @@ class Node{
 
     public:
         Node(); //Normal Constructor
-        Node(int degree, bool isLeaf); //
+        Node(int degree, bool isLeaf); //With degree and isLeaf constructor
 
-        void insertNonFull(Data k);
-        void splitChild(int i, Node*y);
+        void insertNonFull(Data k); //Insert Non Full Function
+        void splitChild(int i, Node*y); //Split child funciton
 
-        void traverse();
-        Node *search(Data k);
+        void traverse(); //Traverse function with pre-fix printing
+        Node *search(Data k); //Returns the adress of node with desired data in it
 friend class Tree;
 };
 
@@ -152,17 +168,18 @@ Node::Node(int degree, bool isLeaf){
     data = new Data[2*degree-1];
     childs = new Node* [2*degree];
 
-    //Number of keys at this point
+    //Number of data at this point
     size = 0;
 
 }
 
 void Node::traverse(){
-
+    //Prints current node data
     for(int i = 0; i < size; i++){
         cout << data[i].get();
         
     }
+    //If this leaf has children, recursively call children nodes
     if(leaf == false){
         for(int i = 0; i<size+1;i++){
             cout << endl;
@@ -181,96 +198,84 @@ Node* Node::search(Data k){
     if(data[i] == k){
         return this;
     }
-
+    //If there is nothing left, end of the tree, return NULL
     if(leaf == true){
         return NULL;
     }
-
+    //Continue with recursively search
     return childs[i]->search(k);
 
 }
-//THIS IS THE ISSUE
+
 void Node::insertNonFull(Data k){
-    // Initialize index as index of rightmost element 
+    // Initialize index as rightmost element 
 	int i = size-1; 
 
 	// If this is a leaf node 
 	if (leaf == true) 
 	{ 
-		// The following loop does two things 
-		// a) Finds the location of new key to be inserted 
-		// b) Moves all greater keys to one place ahead 
+
 		while (i >= 0 && data[i] > k) 
 		{ 
 			data[i+1] = data[i]; 
 			i--; 
 		} 
 
-		// Insert the new key at found location 
 		data[i+1] = k; 
 		size = size+1; 
 	} 
-	else // If this node is not leaf 
+	else
 	{ 
-		// Find the child which is going to have the new key 
-		while (i >= 0 && data[i] > k) 
-			i--; 
 
-		// See if the found child is full 
+		while (i >= 0 && data[i] > k){ 
+			i--;
+        } 
+
 		if (childs[i+1]->size == 2*degree-1) 
 		{ 
-			// If the child is full, then split it 
-			splitChild(i+1, childs[i+1]); 
 
-			// After split, the middle key of C[i] goes up and 
-			// C[i] is splitted into two. See which of the two 
-			// is going to have the new key 
-			if (data[i+1] < k) 
+			splitChild(i+1, childs[i+1]); 
+			if (data[i+1] < k){ 
 				i++; 
+            }
 		} 
 		childs[i+1]->insertNonFull(k); 
 	} 
 }
 
-//NOT THIS FUNCTION
+
 void Node::splitChild(int i, Node *y){
-    // Create a new node which is going to store (t-1) keys 
-	// of y 
+
+    //Creating temporary node
 	Node *z = new Node(y->degree, y->leaf); 
 	z->size = degree - 1; 
 
-	// Copy the last (t-1) keys of y to z 
-	for (int j = 0; j < degree-1; j++) 
-		z->data[j] = y->data[j+degree]; 
+    //Copying data
+	for (int j = 0; j < degree-1; j++){ 
+		z->data[j] = y->data[j+degree];
+    } 
 
-	// Copy the last t children of y to z 
 	if (y->leaf == false) 
 	{ 
-		for (int j = 0; j < degree; j++) 
-			z->childs[j] = y->childs[j+degree]; 
+		for (int j = 0; j < degree; j++){ 
+			z->childs[j] = y->childs[j+degree];
+        } 
 	} 
 
-	// Reduce the number of keys in y 
 	y->size = degree - 1; 
 
-	// Since this node is going to have a new child, 
-	// create space of new child 
-	for (int j = size; j >= i+1; j--) 
-		childs[j+1] = childs[j]; 
+	for (int j = size; j >= i+1; j--){ 
+		childs[j+1] = childs[j];
+    } 
 
-	// Link the new child to this node 
 	childs[i+1] = z; 
 
-	// A key of y will move to this node. Find the location of 
-	// new key and move all greater keys one space ahead 
-	for (int j = size-1; j >= i; j--) 
-		data[j+1] = data[j]; 
+	for (int j = size-1; j >= i; j--){ 
+		data[j+1] = data[j];
+    } 
 
-	// Copy the middle key of y to this node 
 	data[i] = y->data[degree-1]; 
-
-	// Increment count of keys in this node 
-	size = size + 1; 
+	size++; 
 
 }
 
@@ -278,14 +283,14 @@ class Tree{
     private:
         Node *root; //root of node (pointer)
         int degree; //Minumum degree
-		char operationMode;
+		char operationMode; //Operation mode (x for X data, y for Y data, Z for char data)
     
     public:
-        Tree(int degree);
-		Tree(int degree, char operation);
-        void traverse();
-        Node* search(Data k);
-        void insert(int x, int y, char k);
+        Tree(int degree); //Creating tree with data defult operation mode is x
+		Tree(int degree, char operation); //Creating tree with degree and its operation mode
+        void traverse(); //Tree traverse function
+        Node* search(Data k); //Search function
+        void insert(int x, int y, char k); //Insert function
 
 };
 
@@ -301,7 +306,9 @@ Tree::Tree(int degree, char operation){
 }
 
 void Tree::traverse(){
-   if (root != NULL) root->traverse();
+   if (root != NULL) {
+       root->traverse();
+    }
 }
 
 
@@ -311,55 +318,58 @@ Node* Tree::search(Data k){
 
 
 void Tree::insert(int x, int y, char k){
+    //Creating tempory data object with desired inputs
 	Data newData(x, y, k, operationMode);
-	// If tree is empty 
+	
+    // If tree is empty 
 	if (root == NULL) 
 	{ 
-		// Allocate memory for root 
+		// Creating new data and inilization for root 
 		root = new Node(degree, true); 
-		root->data[0] = newData; // Insert key 
-		root->size = 1; // Update number of keys in root 
+		root->data[0] = newData; // Insert data object to root node
+		root->size = 1; // Update number of data in root node 
 	} 
-	else // If tree is not empty 
+	else
 	{ 
-		// If root is full, then tree grows in height 
+		
 		if (root->size == 2*degree-1) 
 		{ 
-			// Allocate memory for new root 
+			//Inilization of memory for new root 
 			Node *s = new Node(degree, false); 
-
-			// Make old root as child of new root 
 			s->childs[0] = root; 
-
-			// Split the old root and move 1 key to the new root 
 			s->splitChild(0, root); 
-
-			// New root has two children now. Decide which of the 
-			// two children is going to have new key 
 			int i = 0; 
-			if (s->data[0] < newData) 
-				i++; 
+			if (s->data[0] < newData){ 
+				i++;
+            } 
 			s->childs[i]->insertNonFull(newData); 
 
-			// Change root 
+			//Root change
 			root = s; 
 		} 
-		else // If root is not full, call insertNonFull for root 
-			root->insertNonFull(newData); 
+		else{ // call insertNonFull for root 
+			root->insertNonFull(newData);
+        } 
 	} 
 }
+//Main function
 int main (){
+    //Gets first input
     int N;
     cin >> N;
     
+    //Gets second input for order
     int order;
     cin >> order;
 
+    //Gets third input for operation mode for comparision of data objects
     char type;
     cin >> type;
 
+    //Creating tree with order and operation mode type
     Tree myTree(order,type);
 
+    //Inserting data to tree
     for(int i = 0; i<N; i++){
         int x,y;
         char z;
@@ -367,5 +377,6 @@ int main (){
         myTree.insert(x,y,z);
     }
     
+    //printig with desired strcutre of print
     myTree.traverse();
 }
